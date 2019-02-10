c9348751b03d4537e0ce3f712595275265b118a4
Drew Harper <dharper998@gmail.com>
2019-02-09 22:46:15 -0500

    Sorta done the things

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
-
+from h1 import main
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/search", methods=['POST'])
+def search():
+    input = request.form['Medical']
+    h1.main(input)
+    return render_template("search.html")
 
 if __name__=="__main__":
     app.run(debug=True)
--- h1.py
+++ h1.py
@@ -16,16 +16,13 @@ def getPublications(articleData):
 def fetchPubs(publicationData):
     finalPublications = []
     for publication in publicationData:
-        #print(publication)
         finalPub = requests.get('http://159.203.144.49:3000/publications?'
         'id=eq.' + publication)
         finalPublications.append(finalPub.json())
     return finalPublications
 
-def main():
-    i = input("Enter a health problem (ex: Cancer, Diabetes): ")
-    articleData = getArticles(i)
+def main(input):
+    articleData = getArticles(input)
     publicationData = getPublications(articleData)
     finalPublications = fetchPubs(publicationData)
     print(finalPublications)
-main()
